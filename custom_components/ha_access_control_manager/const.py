edd586cc8f9779c9512f8f82448c587b961ab2dd
Cory Redmond <ace@ac3-servers.eu>
2026-04-24 22:07:49 +0100

    Change detected in ./custom_components/ha_access_control_manager/const.py

--- custom_components/ha_access_control_manager/const.py
+++ custom_components/ha_access_control_manager/const.py
@@ -7,6 +7,12 @@ DEST_PATH_SCRIPT_JS = "www/community/ha-access-control-manager"
 AUTH_PATH = ".storage/auth"
 NEW_AUTH_PATH = ".storage/auth.new"
 SCRIPT_BASH = f"{BASE_PATH}/www/replace-auth.sh"
+LOVELACE_DASHBOARDS_PATH = ".storage/lovelace_dashboards"
+LOVELACE_STORAGE = ".storage/lovelace"
+LOVELACE_STORAGE_PREFIX = ".storage/lovelace."
+LOVELACE_STORAGE_DIR = ".storage"
+GROUP_DASHBOARD_PERMISSIONS_PATH = ".storage/ha_access_control_manager_dashboards"
+SYSTEM_GROUP_IDS = {"system-admin", "system-users", "system-read-only"}
 
 ICONS = [
     "mdi:lock","mdi:lock-open","mdi:key",
