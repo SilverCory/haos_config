c12ee2bab905ccb3e433415db1f5a5ad136a6059
Cory Redmond <ace@ac3-servers.eu>
2026-04-24 22:07:55 +0100

    Change detected in ./custom_components/ha_access_control_manager/options_flow.py

--- custom_components/ha_access_control_manager/options_flow.py
+++ custom_components/ha_access_control_manager/options_flow.py
@@ -6,8 +6,8 @@ from .const import ICONS
 class OptionsFlowHandler(config_entries.OptionsFlow):
     """Handle options flow for Access Control Manager."""
 
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
+    def __init__(self):
+        self.data = {}
 
     async def async_step_init(self, user_input=None):
         """Manage the options."""
